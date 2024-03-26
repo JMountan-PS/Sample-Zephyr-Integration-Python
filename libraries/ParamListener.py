989791fb574de980974619c04f30962f18ed857d
Jared Mountan <jmountan@copado.com>
2024-03-26 12:52:03 -0400

    changed class name

---
+++ libraries/ParamListener.py
@@ -0,0 +1,32 @@
+from robot.running.model import TestSuite as SuiteData, TestCase as TestData
+from robot.result.model import TestSuite as SuiteResult, TestCase as TestResult
+from robot.api import logger
+import requests
+
+class ParamListener:
+
+    ROBOT_LIBRARY_SCOPE = 'GLOBAL'
+    ROBOT_LISTENER_API_VERSION = 3
+
+    def __init__(self):
+        self.pass_count = 0
+        self.fail_count = 0
+
+    #def start_suite(self, data, result):
+        # Maybe set the suite status as running in Zephyr?
+
+    def end_suite(self, data, result):
+        # Use self.pass_count and self.fail_count and send results
+        testcount = int(self.fail_count) + int(self.pass_count)
+        passrate = int(self.pass_count) / testcount * 100
+        logger.console(f"passrate: {passrate}")
+
+    def start_test(self, data, result):
+        logger.console(f"starting a test {data.name}")
+
+    def end_test(self, data, result):
+        logger.console(f"ending a test {data.name}")
+        if result.status == 'PASS':
+            self.pass_count += 1
+        if result.status == 'FAIL':
+            self.fail_count += 1
