c2ae55f20a4f90595b363181b6802fa45cb659e2
Jared Mountan <jmountan@copado.com>
2024-03-26 13:07:40 -0400

    changed class name

--- libraries/LibraryListener.py
+++ libraries/LibraryListener.py
@@ -19,12 +19,16 @@ class ParamListener:
         # Use self.pass_count and self.fail_count and send results
         testcount = int(self.fail_count) + int(self.pass_count)
         passrate = int(self.pass_count) / testcount * 100
-        logger.console(f"---PARAM LISTENER--- passrate: {passrate}")
+        logger.console("\n")
+        logger.console(f"---LIBRARY LISTENER--- passrate: {passrate}")
+        
 
     def start_test(self, data, result):
+        logger.console("\n")
         logger.console(f"---PARAM LISTENER--- starting a test {data.name}")
 
     def end_test(self, data, result):
+        logger.console("\n")
         logger.console(f"---PARAM LISTENER--- ending a test {data.name}")
         if result.status == 'PASS':
             self.pass_count += 1
